25cdbbe777afc80c026f74bf9f37f3be1dd0090b
Ronaldo Aquino <ronaldoaquinodev@gmail.com>
2021-02-06 16:24:24 -0300

    Componentes com estado

--- src/components/comunicacao/IndiretaFilho.js
+++ src/components/comunicacao/IndiretaFilho.js
@@ -1,8 +1,14 @@
 const IndiretaFilho = ({ fornecerInformacoes, limparInformacoes }) => {
+  const min = 30;
+  const max = 70;
+  const gerarIdade = () => parseInt(Math.random() * ((max - min) + 1)) + min;
+
   return (
     <div>
       <div>Filho</div>
-      <button onClick={() => fornecerInformacoes("Ronaldo", 27, true)}>
+      <button
+        onClick={() => fornecerInformacoes("Ronaldo", gerarIdade(), true)}
+      >
         Fornecer Informações
       </button>
       <button onClick={() => limparInformacoes()}>Limpar Informações</button>
