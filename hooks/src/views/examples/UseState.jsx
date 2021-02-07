36b77b8fc22f59581b7a0d4b88af153c57010286
Ronaldo Aquino <ronaldoaquinodev@gmail.com>
2021-02-06 21:26:02 -0300

    useState 2/2

--- hooks/src/views/examples/UseState.jsx
+++ hooks/src/views/examples/UseState.jsx
@@ -4,6 +4,7 @@ import SectionTitle from "../../components/layout/SectionTitle";
 
 const UseState = (props) => {
   const [count, setCount] = useState(0);
+  const [name, setName] = useState("Inicial...");
 
   const subtract = () => {
     setCount(count - 1);
@@ -41,6 +42,12 @@ const UseState = (props) => {
       </div>
 
       <SectionTitle title="Exercício #02" />
+      <input
+        type="text"
+        className="input"
+        value={name}
+        onChange={(e) => setName(e.target.value)}
+      />
     </div>
   );
 };
