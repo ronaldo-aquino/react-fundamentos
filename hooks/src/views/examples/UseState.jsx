36237788482ce8f0133794851ed8942445b99983
Ronaldo Aquino <ronaldoaquinodev@gmail.com>
2021-02-06 21:16:14 -0300

    useState 1/2

--- hooks/src/views/examples/UseState.jsx
+++ hooks/src/views/examples/UseState.jsx
@@ -1,15 +1,48 @@
-import React from 'react'
-import PageTitle from '../../components/layout/PageTitle'
+import React, { useState } from "react";
+import PageTitle from "../../components/layout/PageTitle";
+import SectionTitle from "../../components/layout/SectionTitle";
 
 const UseState = (props) => {
-    return (
-        <div className="UseState">
-            <PageTitle
-                title="Hook UseState"
-                subtitle="Estado em componentes funcionais!"
-            />
-        </div>
-    )
-}
+  const [count, setCount] = useState(0);
 
-export default UseState
+  const subtract = () => {
+    setCount(count - 1);
+  };
+
+  const sum = () => {
+    setCount(count + 1);
+  };
+
+  return (
+    <div className="UseState">
+      <PageTitle
+        title="Hook UseState"
+        subtitle="Estado em componentes funcionais!"
+      />
+
+      <SectionTitle title="Exercício #01" />
+
+      <div className="center">
+        <span className="text">{count}</span>
+        <div>
+          <button className="btn" onClick={() => subtract()}>
+            -1
+          </button>
+          <button className="btn" onClick={() => sum()}>
+            +1
+          </button>
+          <button
+            className="btn"
+            onClick={() => setCount((current) => current + 1000)}
+          >
+            +1000
+          </button>
+        </div>
+      </div>
+
+      <SectionTitle title="Exercício #02" />
+    </div>
+  );
+};
+
+export default UseState;
