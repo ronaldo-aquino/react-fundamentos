c4e2856774403e8a068ff9a47876f9f3e4848bfb
Ronaldo Aquino <ronaldoaquinodev@gmail.com>
2021-02-08 23:19:31 -0300

    useContext 1/2

---
+++ hooks/src/data/DataContext.js
@@ -0,0 +1,8 @@
+import { createContext } from "react";
+
+export const data = {
+  number: 123,
+  text: "Context API...",
+};
+
+export const DataContext = createContext(data);
--- hooks/src/views/examples/UseContext.jsx
+++ hooks/src/views/examples/UseContext.jsx
@@ -1,15 +1,39 @@
-import React from 'react'
-import PageTitle from '../../components/layout/PageTitle'
+import React, { useContext } from "react";
+import PageTitle from "../../components/layout/PageTitle";
+import { DataContext } from "../../data/DataContext";
 
 const UseContext = (props) => {
-    return (
-        <div className="UseContext">
-            <PageTitle
-                title="Hook UseContext"
-                subtitle="Aceita um objeto de contexto e retorna o valor atual do contexto!"
-            />
-        </div>
-    )
-}
+  const context = useContext(DataContext);
 
-export default UseContext
+  const addNumber = (delta) => {
+    context.setState({
+      ...context.state,
+      number: context.state.number + delta,
+    });
+  };
+
+  return (
+    <div className="UseContext">
+      <PageTitle
+        title="Hook UseContext"
+        subtitle="Aceita um objeto de contexto e retorna o valor atual do contexto!"
+      />
+
+      <div className="center">
+        <span className="text">{context.state.text}</span>
+        <span className="text">{context.state.number}</span>
+
+        <div>
+          <button onClick={() => addNumber(-1)} className="btn">
+            -1
+          </button>
+          <button onClick={() => addNumber(1)} className="btn">
+            +1
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default UseContext;
